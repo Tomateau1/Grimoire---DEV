c205e8e2394e243e0d1e5545080e07937c17d6cd
Mnzo <81849467+Mnzo@users.noreply.github.com>
2026-08-12 00:06:47 +0200

    Switched data to new stats system finished

--- Source/Grimoire/Public/ProjectileData.h
+++ Source/Grimoire/Public/ProjectileData.h
@@ -15,9 +15,7 @@ class GRIMOIRE_API UProjectileData : public UPrimaryDataAsset
 	GENERATED_BODY()
 
 public:
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
-	bool IsPersistent;
-	
+
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
 	float LifeSpan = 5.0;
 	
@@ -31,15 +29,9 @@ public:
 	float TurnRateGrowth = 200.0;
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
-	FVector RelativeRotation = FVector(0.0, 0.0, 0.0);
+	FRotator RelativeRotation = FRotator(0.0, 0.0, 0.0);
 	
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
-	bool DoesApplyBleed = false;
-
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
-	bool DoesApplyBurn = false;
-	
-	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Data")
-	bool DoesApplyFreeze = false;
+	float OuterCurveWidth;
 };
 
